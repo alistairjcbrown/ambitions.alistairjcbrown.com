aed1c94a6f6c8f6f7669ffc159e2552e4ecc43bb
Alistair Brown <github@alistairjcbrown.com>
2014-08-31 00:12:16 +0100

    Added more ambitions and more proofs

--- tests/life.test.js
+++ tests/life.test.js
@@ -11,7 +11,7 @@ suite("Milestones", function() {
             checkThat("I have moved in with my girlfriend", done);
         });
 
-        test("should get a dog", function(done) {
+        test("should get a pet", function(done) {
             checkThat("I have a dog", done);
         });
 
@@ -31,15 +31,23 @@ suite("Milestones", function() {
             checkThat("I am married", done);
         });
 
+        test("should have children", function(done) {
+            checkThat("I have children", done);
+        });
+
     });
 
     suite("Qualifications", function() {
 
-        test("should have completed the Duke of Edinburgh award scheme", function(done) {
+        test("should learn to drive", function(done) {
+            checkThat("I have learnt to drive", done);
+        });
+
+        test("should complete the Duke of Edinburgh award scheme", function(done) {
             checkThat("I have completed the Duke of Edinburgh award scheme", done);
         });
 
-        test("should have a Masters degree", function(done) {
+        test("should graduate with a degree", function(done) {
             checkThat("I have a Masters degree", done);
         });
 
@@ -59,24 +67,144 @@ suite("Milestones", function() {
             checkThat("I have worked as a senior developer", done);
         });
 
+        test("should build something amazing", function(done) {
+            checkThat("I have built something amazing", done);
+        });
+
+        test("should build something successful", function(done) {
+            checkThat("I have built something successful", done);
+        });
+
     });
 
     suite("Travelling", function() {
 
-        test("should go to Cuba", function(done) {
-            checkThat("I have been to Cuba", done);
+        test("should travel around the world", function(done) {
+            checkThat("I have been around the world", done);
+        });
+
+        test("should go to Canada", function(done) {
+            checkThat("I have been to Canada", done);
+        });
+
+        test("should go to America", function(done) {
+            checkThat("I have been to America", done);
+        });
+
+        test("should go to Australia", function(done) {
+            checkThat("I have been to Australia", done);
         });
 
         test("should go to Japan", function(done) {
             checkThat("I have been to Japan", done);
         });
 
+        test("should go to China", function(done) {
+            checkThat("I have been to China", done);
+        });
+
+        test("should go to Thailand", function(done) {
+            checkThat("I have been to Thailand", done);
+        });
+
+        test("should go to all of the Channel Islands", function(done) {
+            checkThat("I have been to all of the Channel Islands", done);
+        });
+
+        test("should go to Germany", function(done) {
+            checkThat("I have been to Germany", done);
+        });
+
+        test("should go to Hungary", function(done) {
+            checkThat("I have been to Hungary", done);
+        });
+
+        test("should go to Norway", function(done) {
+            checkThat("I have been to Norway", done);
+        });
+
+        test("should go to Sweden", function(done) {
+            checkThat("I have been to Sweden", done);
+        });
+
+        test("should go to Iceland", function(done) {
+            checkThat("I have been to Iceland", done);
+        });
+
+        test("should go to Croatia", function(done) {
+            checkThat("I have been to Croatia", done);
+        });
+
+        test("should go to Egypt", function(done) {
+            checkThat("I have been to Egypt", done);
+        });
+
+        test("should go to Mexico", function(done) {
+            checkThat("I have been to Mexico", done);
+        });
+
+        test("should go to Cuba", function(done) {
+            checkThat("I have been to Cuba", done);
+        });
+
+        test("should go to Malaysia", function(done) {
+            checkThat("I have been to Malaysia", done);
+        });
+
+        test("should go to Fiji", function(done) {
+            checkThat("I have been to Fiji", done);
+        });
+
+        test("should go to Maldives", function(done) {
+            checkThat("I have been to Maldives", done);
+        });
+
+        suite("Specific Cities", function() {
+
+            test("should go to London", function(done) {
+                checkThat("I have been to London", done);
+            });
+
+            test("should go to Paris", function(done) {
+                checkThat("I have been to Paris", done);
+            });
+
+            test("should go to New York", function(done) {
+                checkThat("I have been to New York", done);
+            });
+
+            test("should go to Tokyo", function(done) {
+                checkThat("I have been to Tokyo", done);
+            });
+
+        });
+
     });
 
     suite("Miscellaneous", function() {
 
+        test("should attend the midnight premiere of a movie", function(done) {
+            checkThat("I have attended the midnight premiere of a movie", done);
+        });
+
+        test("should get a tattoo", function(done) {
+            checkThat("I have a tattoo", done);
+        });
+
+        test("should have dual passports", function(done) {
+            checkThat("I have dual passports", done);
+        });
+
         test("should get a Japanese toilet", function(done) {
             checkThat("I have a Japanese toilet", done);
+        });
+
+        test("should tell someone \"No\" without giving a reason", function(done) {
+            checkThat("I have told someone \"No\" without giving a reason", done);
+        });
+
+        test("should make something and then try to sell it", function(done) {
+            checkThat("I have made something and tried to sell it", done);
         });
 
         test("should be able to communicate in basic Japanese", function(done) {
